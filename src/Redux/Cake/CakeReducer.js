de5c74c2b1295b8d498041a18338d34efe0ca4f4
Deepak Singh <deepak.singh@growthnatives.co>
2022-07-14 16:04:29 +0530

    create Reducer

---
+++ src/Redux/Cake/CakeReducer.js
@@ -0,0 +1,17 @@
+import { BUY_CAKE } from "./CakeTypes"
+const initialState = {
+    numberOfCake: 10
+}
+
+const CakeReducer = (state = initialState, action) => {
+    switch(action.type) {
+        case BUY_CAKE: return {
+            ...state,
+            numberOfCake: state.numberOfCake - 1
+        }
+
+        default: return state
+    }
+}
+
+export default CakeReducer;
