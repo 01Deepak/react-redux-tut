aa12c6b9f0c628e0de8d95fd3bb96970b5c4641b
Deepak Singh <deepak.singh@growthnatives.co>
2022-07-14 17:08:48 +0530

    add useDispatch in buycake button

--- src/Components/CakeContainer.js
+++ src/Components/CakeContainer.js
@@ -1,12 +1,15 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { buyCake } from '../Redux/Cake/CakeAction'
 
 const CakeContainer = () => {
     const cakes = useSelector(state => state.numberOfCake)
+    const dispatch = useDispatch()
+
   return (
     <div>
         <h2>Number of Cakes - {cakes}</h2>
-        <button>buy</button>
+        <button onClick={() => dispatch(buyCake())}>buy</button>
     </div>
   )
 }
