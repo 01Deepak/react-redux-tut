6fe59ed7b3d81579eb6b733f2bae93cc3cab9cb9
Deepak Singh <deepak.singh@growthnatives.co>
2022-07-14 17:00:19 +0530

    add useSelector hook

--- src/Components/CakeContainer.js
+++ src/Components/CakeContainer.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const CakeContainer = () => {
+    const cakes = useSelector(state => state.numberOfCake)
   return (
     <div>
-        <h2>Number of Cakes</h2>
+        <h2>Number of Cakes - {cakes}</h2>
         <button>buy</button>
     </div>
   )
